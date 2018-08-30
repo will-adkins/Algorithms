3b73dc6917cf3ed84b6194df081e7e27ebc355ab
Will Adkins <wkadki01@gmail.com>
2018-08-30 16:28:52 -0400

    Anagrams and max characters

--- Algorithm Challenges.js
+++ Algorithm Challenges.js
@@ -68,3 +68,54 @@ function reverseInt(n) {
     )
   )
 }
+
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+// ****Anagram, comparing numbers between 2 strings, anything where you need to count characters in a string or values in an array****
+function maxChar(str) {
+  let charMap = {}
+  let max = 0
+  let maxChar = ''
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1
+  }
+
+  for (let key in charMap) {
+    if (charMap[key] > max) {
+      max = charMap[key]
+      maxChar = key
+    }
+  }
+  return maxChar
+}
+
+function isAnagram(str1, str2) {
+  let charMap1 = {}
+  let charMap2 = {}
+  let isAnagram = true
+
+  for (let char of str1) {
+    charMap1[char] = charMap1[char] + 1 || 1
+  }
+  for (let char of str2) {
+    charMap2[char] = charMap2[char] + 1 || 1
+  }
+
+  for (let key in charMap1) {
+    if (charMap1[key] !== charMap2[key]) {
+      isAnagram = false
+    }
+  }
+  for (let key in charMap2) {
+    if (charMap2[key] !== charMap1[key]) {
+      isAnagram = false
+    }
+  }
+  return isAnagram
+}
